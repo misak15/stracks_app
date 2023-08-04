3d98f2c97d479e196bfc5bdcf32fe99ec73a5c8c
misak15 <iami.be.www@gmail.com>
2023-08-04 22:52:39 +0900

    [add]Amplify init
    Amplify init実行

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "stracksamplifye3e4deea": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Trigger8b14b085": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "StrackS": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
